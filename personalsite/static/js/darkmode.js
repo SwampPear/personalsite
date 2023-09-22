b58c3607efe77100ea5fb6cd7f2be972f4a7d584
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-09-22 11:05:37 -0400

    removed welcome scroll arrow - not needed

---
+++ personalsite/static/js/darkmode.js
@@ -0,0 +1,22 @@
+let darkMode = false
+
+const darkModeButton = document.querySelector('.nav__darmode-button')
+
+const toDarkMode = () => {
+
+}
+
+const fromDarkMode = () => {
+
+}
+
+darkModeButton.addEventListener('click', () => {
+
+})
+
+/*
+- body
+- link buttons
+- darkmode button
+
+*/
