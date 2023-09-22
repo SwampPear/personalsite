ebb8ad74b3b50d1c93bde47e32d09b64e36e5203
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-09-22 12:01:04 -0400

    removed color funcitonality from link hover

--- personalsite/static/js/darkmode.js
+++ personalsite/static/js/darkmode.js
@@ -1,18 +1,51 @@
 let darkMode = false
 
-const darkModeButton = document.querySelector('.nav__darmode-button')
+const darkModeButton = document.querySelector( '.nav__darkmode-button' )
+
+const body = document.querySelector( 'body' )
+const navLinkBrand = document.querySelector( '.nav__link__brand' )
+const navLinks = document.querySelectorAll( '.nav__link' )
+
 
 const toDarkMode = () => {
+    body.classList.remove( 'body__from-darkmode' )
+    body.classList.add( 'body__to-darkmode' )
 
+    navLinkBrand.classList.remove( 'nav__link__from-darkmode' )
+    navLinkBrand.classList.add( 'nav__link__to-darkmode' )
+
+    navLinks.forEach( ( elem ) => {
+        elem.classList.remove( 'nav__link__from-darkmode' )
+        elem.classList.add( 'nav__link__to-darkmode' )
+    })
 }
 
 const fromDarkMode = () => {
+    body.classList.remove( 'body__to-darkmode' )
+    body.classList.add( 'body__from-darkmode' )
 
+    navLinkBrand.classList.remove( 'nav__link__to-darkmode' )
+    navLinkBrand.classList.add( 'nav__link__from-darkmode' )
+
+    navLinks.forEach( ( elem ) => {
+        elem.classList.remove( 'nav__link__to-darkmode' )
+        elem.classList.add( 'nav__link__from-darkmode' )
+    })
 }
 
-darkModeButton.addEventListener('click', () => {
+const toggleDarkmode = () => {
+    if ( !darkMode ) {
+        toDarkMode()
 
-})
+        darkMode = true
+    } else {
+        fromDarkMode()
+
+        darkMode = false
+    }
+}
+
+darkModeButton.addEventListener( 'click', toggleDarkmode)
 
 /*
 - body
